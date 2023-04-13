a34aa0c62cd831f81d6226833f351e152815f5dc
bradenasmith2 <bradenasmith@icloud.com>
2023-04-13 11:02:59 -0500

    Braden S M1W4 Assessment 4/13

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -12,17 +12,31 @@ namespace Vehicle.UnitTests
             Assert.Equal(0, megansCar.MilesDriven);
         }
 
-        // This test needs some work!
         [Fact]
         public void Summary_ReturnsInfoAboutVehicle()
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
+            var expected = "This Blue vehicle has 2 wheels, and has driven 0 miles."; // , and . added
 
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
-
-
+            Assert.Equal(expected, andysBike.Sumary());
         }
 
-        // Add more tests here!
+        [Fact]
+        public void Drive_AddsFive_To_MilesDriven_WithCorrectValue()
+        {
+            Vehicle vehicle1 = new(4, "Red");
+            vehicle1.Drive();
+
+            Assert.Equal(5, vehicle1.MilesDriven);
+        }
+
+        [Fact]
+        public void Paint_Gets_NewColor_AndSets_ColorToNewColor()
+        {
+            Vehicle vehicle1 = new(4, "Red");
+            vehicle1.Paint("Blue");
+
+            Assert.Equal("Blue", vehicle1.Color);
+        }
     }
 }
